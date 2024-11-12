6f009000243b2e159c174fe5feb2dcb67cb377b2
naygharbi <naylagharbi@outlook.es>
2024-11-12 12:11:35 +0100

    Feature: Javascript press typeof numero, cadena, boolean ex02

---
+++ Javascript/sheet01/ex02/script.js
@@ -0,0 +1,7 @@
+let numero = 42;
+let cadena = "Hola, Mundo";
+let booleano = true;
+
+console.log(`Valor: ${numero}, Tipo: ${typeof numero}`);
+console.log(`Valor: ${cadena}, Tipo: ${typeof cadena}`);
+console.log(`Valor: ${booleano}, Tipo: ${typeof booleano}`);
