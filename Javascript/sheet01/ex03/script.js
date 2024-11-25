96da1bbbcba117658f2df695168fa57814c504cc
naygharbi <naylagharbi@outlook.es>
2024-11-25 09:49:42 +0100

    Operaciones Matemáticas Básicas

---
+++ Javascript/sheet01/ex03/script.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 5;
+
+console.log(`suma: ${a + b}`);
+console.log(`resta: ${a - b}`);
+console.log(`multiplicación: ${a * b}`);
+console.log(`división: ${a / b}`);
+console.log(`resto: ${a % b}`);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
