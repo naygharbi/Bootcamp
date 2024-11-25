a3a8a1b1a677ba634e289b73ed0486ba4d30ad91
naygharbi <naylagharbi@outlook.es>
2024-11-25 09:53:29 +0100

    Condicionales

---
+++ Javascript/sheet01/ex04/script.js
@@ -0,0 +1,9 @@
+let numero = -1; // Puedes cambiar este número para probar diferentes casos
+
+if (numero > 0) {
+  console.log("El número es positivo");
+} else if (numero < 0) {
+  console.log("El número es negativo");
+} else {
+  console.log("El número es cero");
+}
