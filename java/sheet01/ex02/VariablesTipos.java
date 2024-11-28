e8c3b0cdb613f296be6a262d007dfbadfde77be5
naygharbi <naylagharbi@outlook.es>
2024-11-28 12:13:46 +0100

    feat: java ex02 variables y tipos

---
+++ java/sheet01/ex02/VariablesTipos.java
@@ -0,0 +1,15 @@
+class VariablesTipos{
+	public static void main(String[] args) {
+		int numEntero = 23;
+		double numDecimal = 23.69;
+		boolean verdadero= true;
+		char caracter = 'C';
+		String cadena= "Nayla";
+	
+	System.out.println("Valor de Int:" + numEntero);	
+	System.out.println("Valor de double:" + numDecimal);
+	System.out.println("Valor de boolean:" + verdadero);
+	System.out.println("Valor de caracter:" + caracter);
+	System.out.println("Valor de string:" + cadena);
+	}
+}
