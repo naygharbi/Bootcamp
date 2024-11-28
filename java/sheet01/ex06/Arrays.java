d1cad1e6152fd67947c74cae1dccb3d30947fd7c
naygharbi <naylagharbi@outlook.es>
2024-11-28 13:59:12 +0100

    feat: ex06 Arrays

---
+++ java/sheet01/ex06/Arrays.java
@@ -0,0 +1,11 @@
+
+public class Arrays {
+	public static void main(String[] args) {
+		int [] array = {5, 6, 4, 3, 9};
+		for (int i = 0; i < array.length; i++) {
+			System.err.println(array[i]);
+		}
+	}
+}
+
+// for (numbers : array[])
