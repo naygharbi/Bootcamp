83763510105859f83181540c6c6cdbe23d69d2bb
naygharbi <naylagharbi@outlook.es>
2024-11-29 12:31:38 +0100

    ex07

---
+++ java/sheet01/ex07/Funcciones.java
@@ -0,0 +1,10 @@
+public class Funcciones {
+	public static void main(String[] args) {
+		int resultado = suma(5,9);
+		System.out.println("Suma es: " + resultado);
+	}
+
+	public static int suma(int a, int b) {
+		return a+b;
+	} 
+}
